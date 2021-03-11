39264009a1e4469ea2a2cfd5a3466c4fcb420bf4
TFNeto <tiago8filipe@gmail.com>
2021-03-11 21:14:40 +0000

    fixed discount applier with mock - multiplicatores were changed

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,38 @@
 package tudelft.discount;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiscountApplierTest {
 
+
+    @Test
+    public void TestBusinessProduct(){
+        ProductDao MockedDao = Mockito.mock(ProductDao.class);
+        List<Product> ProductsList = new ArrayList<Product>();
+
+        ProductsList.add(new Product("Phone",100, "BUSINESS"));
+
+        Mockito.when(MockedDao.all()).thenReturn(ProductsList);
+        DiscountApplier discounts= new DiscountApplier(MockedDao);
+        discounts.setNewPrices();
+
+        Assertions.assertEquals(110,ProductsList.get(0).getPrice(),0.00001);
+    }
+    @Test
+    public void TestHomeProduct(){
+        ProductDao MockedDao = Mockito.mock(ProductDao.class);
+        List<Product> ProductsList = new ArrayList<Product>();
+
+        ProductsList.add(new Product("Phone",100, "HOME"));
+
+        Mockito.when(MockedDao.all()).thenReturn(ProductsList);
+        DiscountApplier discounts= new DiscountApplier(MockedDao);
+        discounts.setNewPrices();
+
+        Assertions.assertEquals(90,ProductsList.get(0).getPrice(),0.00001);
+    }
 }
